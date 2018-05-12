9f0d4329f5b229e223c82d30dfcdf2deb0459d3b
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-05-12 19:10:46 +1000

    Remove NuGet

--- Assets/App/Tests/Editor/TestPlayer.cs
+++ Assets/App/Tests/Editor/TestPlayer.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+#if UNITY_EDITOR_NOPE
 using UnityEngine.TestTools;
+#endif
 using NUnit.Framework;
 using System.Collections;
 
@@ -10,12 +12,12 @@ public class NewTestScript {
         // Use the Assert class to test conditions.
     }
 
-    // A UnityTest behaves like a coroutine in PlayMode
-    // and allows you to yield null to skip a frame in EditMode
-    [UnityTest]
-    public IEnumerator NewTestScriptWithEnumeratorPasses() {
-        // Use the Assert class to test conditions.
-        // yield to skip a frame
-        yield return null;
-    }
+    //// A UnityTest behaves like a coroutine in PlayMode
+    //// and allows you to yield null to skip a frame in EditMode
+    //[UnityTest]
+    //public IEnumerator NewTestScriptWithEnumeratorPasses() {
+    //    // Use the Assert class to test conditions.
+    //    // yield to skip a frame
+    //    yield return null;
+    //}
 }
