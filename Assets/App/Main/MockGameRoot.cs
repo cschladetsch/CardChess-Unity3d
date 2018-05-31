9c2c5f5ca5e4a6bf60991e1f26e26710d3124aef
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-05-31 18:42:23 +1000

    Add MockGameRoot
    Former-commit-id: 449d6a095b7230a49359bfd098e0688776ea6cce

---
+++ Assets/App/Main/MockGameRoot.cs
@@ -0,0 +1,70 @@
+﻿using App.Agent;
+using App.Common;
+using App.Mock;
+using App.Mock.Model;
+using App.Model;
+using UnityEngine;
+
+// field not assigned - because it is assigned in Unity3d editor
+#pragma warning disable CS0649
+namespace App
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// The intended root of all non-canvas objects in the scene.
+    /// </summary>
+    class MockGameRoot : View.Impl.ViewBase
+    {
+        /// <summary>
+        /// What makes the decisions.
+        /// </summary>
+        public ArbiterGameObject Arbiter;
+
+        public GameObject[] Startup;
+
+        protected override bool Create()
+        {
+            base.Create();
+
+            PrepareModels();
+            PrepareAgents();
+            PrepareViews();
+
+            return true;
+        }
+
+        void PrepareModels()
+        {
+            _models = new ModelRegistry();
+            _models.Bind<Service.ICardTemplateService, Service.Impl.CardTemplateService>();
+            _models.Bind<IBoardModel, BoardModel>(new BoardModel(8, 8));
+            _models.Bind<IArbiterModel, ArbiterModel>(new ArbiterModel());
+            _models.Bind<IWhitePlayerModel, WhitePlayerModel>();
+            _models.Bind<IBlackPlayerModel, BlackPlayerModel>();
+            _models.Bind<ICardModel, CardModel>();
+            _models.Bind<IDeckModel, MockDeck>();
+            _models.Bind<IHandModel, MockHand>();
+            _models.Bind<IPieceModel, PieceModel>();
+            _models.Resolve();
+        }
+
+        private void PrepareAgents()
+        {
+            _agents = new AgentRegistry();
+            _agents.Bind<IArbiterAgent, ArbiterAgent>();
+            _agents.Bind<IBoardAgent, BoardAgent>();
+            _agents.Bind<ICardAgent, CardAgent>();
+            _agents.Bind<IDeckAgent, DeckAgent>();
+        }
+
+        private void PrepareViews()
+        {
+        }
+
+        private App.Model.ModelRegistry _models;
+        private App.Agent.AgentRegistry _agents;
+        private App.View.ViewRegistry _views;
+
+
+    }
+}
