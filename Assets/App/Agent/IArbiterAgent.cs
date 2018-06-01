9db12d737d4493e109f30edc6a61997be60f6b9b
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-06-02 01:34:33 +1000

    Code review, format

--- Assets/App/Agent/IArbiterAgent.cs
+++ Assets/App/Agent/IArbiterAgent.cs
@@ -1,11 +1,16 @@
 ﻿using App.Model;
 using Flow;
+using UniRx;
 
 namespace App.Agent
 {
     public interface IArbiterAgent
         : IAgent<IArbiterModel>
     {
+        IReadOnlyReactiveProperty<IPlayerAgent> PlayerAgent { get; }
+        IReadOnlyReactiveProperty<IPlayerModel> Player { get; }
+        IBoardAgent BoardAgent { get; }
+
         void StartGame(IPlayerAgent p0, IPlayerAgent p1);
         ITransient NewGame();
         void Step();
