9340070691d355f53a598637bc1d037a1967dfa2
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-06-02 23:14:24 +1000

    Add reactive subscription to distinct square selection changes to BoardView

--- Assets/App/View/Impl/SquareView.cs
+++ Assets/App/View/Impl/SquareView.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using App.Common;
+using UniRx;
+using UnityEngine;
 
 #pragma warning disable 649
 
@@ -14,5 +16,53 @@ namespace App.View.Impl
     {
         public EColor Color;
         public float Length;
+        public Coord Coord;
+
+        public IReadOnlyReactiveProperty<bool> MouseOver => _mouseOver;
+
+        protected override void Begin()
+        {
+            _mouseOver = new BoolReactiveProperty(false);
+            base.Begin();
+            //_layerBitMask = 1 << gameObject.layer;//LayerMask.NameToLayer("BoardSquare");
+        }
+
+        protected override void Step()
+        {
+            base.Step();
+        }
+
+        public void EnterMouseOver()
+        {
+            Info("Enter {this}");
+            _mouseOver.Value = true;
+        }
+
+        public override int GetHashCode()
+        {
+            return Coord.GetHashCode();
+        }
+
+        public override bool Equals(object other)
+        {
+            var sq = other as SquareView;
+            if (sq == null)
+                return false;
+            return Coord == sq.Coord;
+        }
+
+        public void LeaveMouseOver()
+        {
+            Info("Leave {this}");
+            _mouseOver.Value = false;
+        }
+
+        public override string ToString()
+        {
+            return $"{Color}-square at {Coord} (WP={transform.position})";
+        }
+
+        private int _layerBitMask;
+        private BoolReactiveProperty _mouseOver;
     }
 }
