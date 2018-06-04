7879a41becf7e9536c6964fc1e777c38583c7765
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-06-04 17:52:46 +1000

    Add Player and Deck views, completing basic gameboard layout

---
+++ Assets/App/View/Impl1/PlayerView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Agent;
+
+namespace App.View.Impl1
+{
+    public class PlayerView
+        : ViewBase<IPlayerAgent>
+        , IPlayerView
+    {
+        public ManaBar ManaBar;
+        public IHandView Hand;
+        public IDeckView Deck;
+    }
+}
