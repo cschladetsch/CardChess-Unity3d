6c69b8d219bf935c30c41c08035b2d7246099fe1
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-06-09 00:27:14 +1000

    First dragging of cards

---
+++ Assets/App/View/Impl1/CardBackgroundView.cs
@@ -0,0 +1,72 @@
+﻿using App.Common;
+using UnityEngine;
+using App.View.Impl1;
+using CoLib;
+
+namespace App.View
+{
+    public class CardBackgroundView
+        : ViewBase
+    {
+        protected override void Begin()
+        {
+            base.Begin();
+            _cardView = transform.parent.GetComponent<CardView>();
+            Assert.IsNotNull(_cardView);
+            _cardTransform = _cardView.transform;
+            _renderer = GetComponent<Renderer>();
+            _backgroundColor = new Ref<Color>(() => _renderer.material.color, c => _renderer.material.color = c);
+
+            _renderer.material.color = new Color(0,0,0,0);
+        }
+
+        double duration = 0.23;
+
+        void OnMouseDown()
+        {
+            var c = _renderer.material.color;
+            c.a = 0;
+            _renderer.material.color = c;
+            startLocation = _cardTransform.position;
+            screenPoint = Camera.main.WorldToScreenPoint(_cardTransform.position);
+            offset = _cardTransform.position -
+                     Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+                         screenPoint.z));
+            _Queue.Enqueue(
+                Commands.AlphaTo(_backgroundColor, 0, duration, Ease.Smooth())
+            );
+        }
+
+        void OnMouseDrag()
+        {
+            Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+            _cardTransform.position = cursorPosition;
+        }
+
+        void OnMouseUp()
+        {
+            _Queue.Enqueue(
+                Commands.Parallel(
+                    Commands.AlphaTo(_backgroundColor, 1, duration, Ease.Smooth()),
+                    Commands.MoveTo(
+                        _cardView.transform,
+                        startLocation,
+                        0.23,
+                        Ease.Smooth()
+                    )
+                )
+            );
+        }
+
+        private Vector3 startLocation;
+        private Vector3 screenPoint;
+        private Vector3 offset;
+        private CardView _cardView;
+        private Transform _cardTransform;
+
+        private Ref<Color> _backgroundColor;
+        private Renderer _renderer;
+    }
+}
+
--- Assets/App/View/Impl1/CardView.cs
+++ Assets/App/View/Impl1/CardView.cs
@@ -1,6 +1,8 @@
 ﻿using App.Agent;
+using CoLib;
 using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace App.View.Impl1
 {
@@ -11,25 +13,62 @@ namespace App.View.Impl1
         public TMPro.TextMeshProUGUI Mana;
         public TMPro.TextMeshProUGUI Health;
         public TMPro.TextMeshProUGUI Power;
-        public GameObject ModelPrefab;
-        public float Width;
+        public Image Image;
 
         public override void Create()
         {
+            _backgroundColor = new Ref<Color>(() => Image.color, c => Image.color = c);
         }
 
         public override void SetAgent(ICardAgent agent)
         {
-            #if UNITY_EDITOR
-            if (agent == null)
-                return;
-            #endif
-
             base.SetAgent(agent);
 
             agent.Power.DistinctUntilChanged().Subscribe(p => Power.text = $"{p}");
             agent.Health.DistinctUntilChanged().Subscribe(p => Health.text = $"{p}");
             agent.Model.ManaCost.DistinctUntilChanged().Subscribe(p => Mana.text = $"{p}");
         }
+
+
+        void OnMouseDown()
+        {
+            _startLocation = transform.position;
+            _screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+            _offset = transform.position - Camera.main.ScreenToWorldPoint(
+                new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z));
+
+            _Queue.Enqueue(
+                Commands.AlphaTo(_backgroundColor, 0, _imageAlphaAnimDuration, Ease.Smooth())
+            );
+        }
+
+        void OnMouseDrag()
+        {
+            var cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z);
+            var cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + _offset;
+            transform.position = cursorPosition;
+        }
+
+        void OnMouseUp()
+        {
+            _Queue.RunToEnd();
+            _Queue.Enqueue(
+                Commands.Parallel(
+                    Commands.AlphaTo(_backgroundColor, 1, _imageAlphaAnimDuration, Ease.Smooth()),
+                    Commands.MoveTo(
+                        transform,
+                        _startLocation,
+                        0.23,
+                        Ease.Smooth()
+                    )
+                )
+            );
+        }
+
+        private Vector3 _startLocation;
+        private Vector3 _screenPoint;
+        private Vector3 _offset;
+        private double _imageAlphaAnimDuration = 0.5;
+        private Ref<Color> _backgroundColor;
     }
 }
