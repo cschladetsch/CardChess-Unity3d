44a70c326eb4db8dd5af791a2b8dcdd5456ac07d
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-06-21 20:24:02 +1000

    Add Free  Chess set

---
+++ Assets/External/Arcane Cyber/Chess/Demo/Scripts/CameraRotation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+public sealed class CameraRotation : MonoBehaviour {
+
+    public Transform target;
+
+    private float xAngle = 0.2f;
+    private float zoom = 0f;
+    
+    private const float MAX_ROTATION_SPEED = 1f;
+    private const float ZOOM_SPEED = 2f;
+
+
+    private void Update() {
+        if (!target)
+            return;
+
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) {
+            xAngle = Input.GetAxis("Mouse X");
+
+            if (xAngle > MAX_ROTATION_SPEED)
+                xAngle = MAX_ROTATION_SPEED;
+            else if (xAngle < -MAX_ROTATION_SPEED)
+                xAngle = -MAX_ROTATION_SPEED;
+        }
+        zoom = Input.GetAxis("Mouse ScrollWheel") * ZOOM_SPEED;
+
+        Debug.Log(xAngle);
+
+        transform.LookAt(target);
+        transform.Translate(new Vector3(xAngle, 0f, zoom) );
+    }
+
+}
---
+++ Assets/External/Arcane Cyber/Chess/readme.txt
@@ -0,0 +1,22 @@
+Hi,
+Chess is one of my favourite games, and that is why I made it for free.
+
+
+Model Name					Triangle Count
+Chess Board					628
+Bishop						2836
+King						2096
+Knight						2914
+Pawn						2302
+Queen						3036
+Rook						1704
+
+
+The marble texture is created by Amada44 and modified by Arcane Cyber.
+https://commons.wikimedia.org/wiki/User:Amada44
+
+If you like it, check out my other products in the Asset Store:
+https://www.assetstore.unity3d.com/en/#!/query=publisher:18730
+
+If you have any questions feel free to contact me at
+arcanecyber@gmail.com
