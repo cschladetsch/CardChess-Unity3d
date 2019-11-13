3ecff32f8ad67a4381a6ce5be4a1e15ab40b688e
Christian Schladetsch <christian.schladetsch@gmail.com>
2019-11-13 23:58:05 +1100

    Update TestAgent.cs
    Remove dead code.
    
    Former-commit-id: 710e3d9dbade5dc05e41ddcd01f2007112c74304

--- Assets/App/Test/Editor/TestAgent.cs
+++ Assets/App/Test/Editor/TestAgent.cs
@@ -8,62 +8,11 @@ namespace App.Agent.Test
     using App;
     using Mock;
     using Mock.Agent;
-    /*
-    class TestAgentBase<TWhite, TBlack>
-        : Model.Test.TestBaseModel
-        where TBlack : IBlackPlayerAgent
-        where TWhite : IWhitePlayerAgent
-    {
-        protected TestAgentBase()
-        {
-            LogSubject = this;
-            LogPrefix = "AgentTest";
-        }
-
-        protected override void SetupTest()
-        {
-            base.SetupTest();
-
-            _agency = new AgentRegistry();
-            _agency.Bind<IBoardAgent, BoardAgent>(new BoardAgent(_board));
-            _agency.Bind<IArbiterAgent, ArbiterAgent>(new ArbiterAgent(_arbiter));
-            _agency.Bind<ICardAgent, CardAgent>();
-            _agency.Bind<IDeckAgent, DeckAgent>();
-            _agency.Bind<IHandAgent, HandAgent>();
-            _agency.Bind<IPieceAgent, PieceAgent>();
-            _agency.Bind<IWhitePlayerAgent, TWhite>();
-            _agency.Bind<IBlackPlayerAgent, TBlack>();
-            _agency.Resolve();
-
-            _boardAgent = _agency.New<IBoardAgent>();
-            _arbiterAgent = _agency.New<IArbiterAgent>();
-            _whiteAgent = _agency.New<IWhitePlayerAgent>(_white);
-            _blackAgent = _agency.New<IBlackPlayerAgent>(_black);
-        }
-
-        protected override void TearDownTest()
-        {
-            _boardAgent.Destroy();
-            _arbiterAgent.Destroy();
-            _blackAgent.Destroy();
-            _whiteAgent.Destroy();
-
-            base.TearDownTest();
-        }
-
-        protected AgentRegistry _agency;
-        protected IBoardAgent _boardAgent;
-        protected IPlayerAgent _whiteAgent;
-        protected IPlayerAgent _blackAgent;
-        protected IArbiterAgent _arbiterAgent;
-    }
-    */
-
+    
+    // Test a game played with designated moves
     [TestFixture]
-    class TestAgent0 : //TestAgentBase<MockWhitePlayerAgent, MockBlackPlayerAgent>
+    class TestAgent0 :
         Model.Test.TestBaseModel
-        //where TBlack : IBlackPlayerAgent
-        //where TWhite : IWhitePlayerAgent
     {
         public TestAgent0()
         {
@@ -119,7 +68,6 @@ namespace App.Agent.Test
             for (int n = 0; n < 100; ++n)
             {
                 _arbiterAgent.Step();
-                //Info($"{_arbiterAgent.Kernel.Root}");
                 Info(_board.Print());
                 if (_arbiter.GameState.Value == EGameState.Completed)
                     break;
