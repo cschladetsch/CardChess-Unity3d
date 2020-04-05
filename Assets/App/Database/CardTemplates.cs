904e06ec4b03ab7d554d618926d1561e4224a173
Christian <christian.schladetsch@gmail.com>
2020-04-05 12:01:32 +1000

    Fix unit tests due to new card template library.

--- Assets/App/Database/CardTemplates.cs
+++ Assets/App/Database/CardTemplates.cs
@@ -10,7 +10,7 @@
     /// <summary>
     /// Contains all the card templates.
     /// </summary>
-    internal static class CardTemplates
+    public static class CardTemplates
     {
         private static readonly Dictionary<string, ICardTemplate> _templates = new Dictionary<string, ICardTemplate>();
 
@@ -55,28 +55,31 @@
             return _templates.Select(kv => kv.Value).Where(template => template.PieceType == type);
         }
 
-		// private static void AddBasicCards()
-  //       {
-  //           _templates.Clear();
-  //           CardTemplate[] cards =
-  //           {                                                                        //M, A, H
-  //               new CardTemplate(ECardType.Piece, EPieceType.Peon, "Peon",           1, 1, 1),       // pawn
-  //               new CardTemplate(ECardType.Piece, EPieceType.King, "King",           1, 1, 20),       // king
-  //               new CardTemplate(ECardType.Piece, EPieceType.Queen, "Queen",         5, 5, 5),
-  //               //new CardTemplate(ECardType.Piece, EPieceType.Paladin, "Paladin",     1, 1, 2),        // pawn after Barracks
-  //               new CardTemplate(ECardType.Piece, EPieceType.Gryphon, "Gryphon",     2, 1, 2),//, card new[] {EAbility.Mountable, EAbility.Lethal}),      // knight
-  //               new CardTemplate(ECardType.Piece, EPieceType.Archer, "Archer",       3, 2, 3),        // bishop
-  //               //new CardTemplate(ECardType.Piece, EPieceType.Castle, "Cannon",       4, 5, 6, new[] {EAbility.Guard}),
-  //               //new CardTemplate(ECardType.Piece, EPieceType.Barricade, "Barricade", 2, 0, 3, new[] {EAbility.Guard, EAbility.Static}),
-  //               //new CardTemplate(ECardType.Piece, EPieceType.Siege, "Siege",         3, 2, 1, new[] {EAbility.Static}),
-  //               //new CardTemplate(ECardType.Piece, EPieceType.Dragon, "Dragon",       9, 8, 5, new[] {EAbility.Mountable}),
-  //           };
-  //
-  //           foreach (var card in cards)
-  //           {
-  //               _templates[card.Id] = card;
-  //           }
-  //       }
+        /// <summary>
+        /// Used for unit tests.
+        /// </summary>
+		public static void AddBasicCards()
+        {
+            _templates.Clear();
+            CardTemplate[] cards =
+            {                                                                        //M, A, H
+                new CardTemplate(ECardType.Piece, EPieceType.Peon, "Peon", 1, 1, 1),       // pawn
+                new CardTemplate(ECardType.Piece, EPieceType.King, "King", 1, 1, 20),       // king
+                new CardTemplate(ECardType.Piece, EPieceType.Queen, "Queen", 5, 5, 5),
+                //new CardTemplate(ECardType.Piece, EPieceType.Paladin, "Paladin",     1, 1, 2),        // pawn after Barracks
+                new CardTemplate(ECardType.Piece, EPieceType.Gryphon, "Gryphon", 2, 1, 2),//, card new[] {EAbility.Mountable, EAbility.Lethal}),      // knight
+                new CardTemplate(ECardType.Piece, EPieceType.Archer, "Archer", 3, 2, 3),        // bishop
+                //new CardTemplate(ECardType.Piece, EPieceType.Castle, "Cannon",       4, 5, 6, new[] {EAbility.Guard}),
+                //new CardTemplate(ECardType.Piece, EPieceType.Barricade, "Barricade", 2, 0, 3, new[] {EAbility.Guard, EAbility.Static}),
+                //new CardTemplate(ECardType.Piece, EPieceType.Siege, "Siege",         3, 2, 1, new[] {EAbility.Static}),
+                //new CardTemplate(ECardType.Piece, EPieceType.Dragon, "Dragon",       9, 8, 5, new[] {EAbility.Mountable}),
+            };
+  
+            foreach (var card in cards)
+            {
+                _templates[card.Id.ToString()] = card;
+            }
+        }
 
         public static ICardTemplate GetRandom()
         {
