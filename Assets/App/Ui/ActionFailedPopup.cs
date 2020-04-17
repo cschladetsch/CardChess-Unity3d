f036bb77be9c7bc151385ddc1e9c10bb0b260c8e
Christian <christian.schladetsch@gmail.com>
2020-04-17 11:21:05 +1000

    Add ActionFailedPopup WIP

---
+++ Assets/App/Ui/ActionFailedPopup.cs
@@ -0,0 +1,18 @@
+﻿namespace App
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using Dekuple.View;
+
+    /// <summary>
+    /// What this class does and how it works with other classes.
+    /// </summary>
+    public class ActionFailedDialog
+        : 
+    {
+        
+    }
+}
+
