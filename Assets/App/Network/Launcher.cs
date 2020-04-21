c5693de84c611f81d1b3ed058f2aa04f7b1c3b71
Christian <christian.schladetsch@gmail.com>
2020-04-21 23:07:56 +1000

    Add App.Network namespace.

---
+++ Assets/App/Network/Launcher.cs
@@ -0,0 +1,28 @@
+﻿namespace App
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// What this class does and how it works with other classes.
+    /// </summary>
+    public class Launcher
+        : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
+
